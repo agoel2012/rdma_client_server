94e5df8569dabd03a499f8ad3259b8b825d89237
Arnav Goel <arnav.goel@sambanovasystems.com>
2023-07-01 20:23:41 +0000

    Bootstrap server code with skeleton lib code

--- rdma_server.c
+++ rdma_server.c
@@ -14,18 +14,24 @@
 
 int start_server(server_info_t *sv) {
 
-    int rc = 0;
-
     // Setup Server control plane
     server_ctx_t *ctx = setup_server(sv->ip_addr, sv->app_port);
     API_NULL(
         ctx, { return (-1); }, "Server Setup Failed\n");
 
+    // Prepare request/response structures
+    API_STATUS(
+        prepare_server_data(ctx), { return -1; },
+        "Unable to prepare the server request data\n");
+
     while (ctx->is_connected) {
-        // TODO: Add recv/send information
+        API_STATUS(
+            send_recv_server(ctx), { return -1; },
+            "Unable to send/recv request/response to/from server\n");
+        // TODO: Define stopping condition ?
     }
 
-    return (rc);
+    return (0);
 }
 
 int main(int argc, char *argv[]) {
